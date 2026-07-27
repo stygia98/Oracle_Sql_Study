544e65c6db81af45f085d724e81d94b4bde08cc4
stygia98 <stygia98@gmail.com>
2026-07-27 17:29:43 +0900

    [Oracle] 계정 권한 명령

--- 00system_create_account.sql
+++ 00system_create_account.sql
@@ -1,3 +1,21 @@
+Drop user springuser CASCADE;
+
+
+
+-- 사용자 설정 (id javauser / pw javauser / tablespace firstData)
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+
+create user javauser IDENTIFIED by javauser
+default TABLESPACE firstData
+TEMPORARY TABLESPACE temp;
+
+-- 사용자 권한 설정 (javauser 접속권한)
+grant CONNECT, RESOURCE TO javauser;
+
+-- 접속요청 진행
+
+
+
 -- 사용자 설정 (id javauser / pw javauser / tablespace firstData)
 ALTER SESSION SET "_ORACLE_SCRIPT"=true;
 
@@ -7,5 +25,19 @@ TEMPORARY TABLESPACE temp;
 
 -- 사용자 권한 설정 (javauser 접속권한)
 grant CONNECT, RESOURCE, dba TO springuser;
+
+-- 접속요청 진행
+
+
+
+-- 사용자 설정 (id javauser / pw javauser / tablespace firstData)
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+
+create user springboard IDENTIFIED by springboard
+default TABLESPACE users
+TEMPORARY TABLESPACE temp;
+
+-- 사용자 권한 설정 (javauser 접속권한)
+grant CONNECT, RESOURCE, dba TO springboard;
 
 -- 접속요청 진행
